2f1200303d0f42c0d1c245df4e26fb94a4fe4578
kenta.yabe <kenta.yabe.0127@gmail.com>
2020-08-24 05:31:49 +0000

    book_button:create by from_with hidden

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,14 @@ class FavoritesController < ApplicationController
 
 
   def create
-    micropost = Micropost.find(params[:id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.book(micropost)
     flash[:success] = '投稿をお気に入りに登録しました。'
     redirect_to current_user
   end
 
   def destroy
-    micropost = Micropost.find(params[:id])
+    micropost = Micropost.find(params[:micropost_id])
     current_user.unbook(micropost)
     flash[:success] = '投稿のお気に入り登録を解除しました。'
     redirect_to current_user
